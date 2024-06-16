f3911a68a70a8a838306188580ec1bc20862d660
Carnis <lapo@golfomistico.com>
2024-06-16 03:59:03 +0200

    railway prep

--- ppm_site/settings.py
+++ ppm_site/settings.py
@@ -123,11 +123,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-MEDIA_ROOT = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = 'app/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'app/media')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'media'),
+    os.path.join(BASE_DIR, 'app/media'),
 )
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
